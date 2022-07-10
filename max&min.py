2fba23854cfd129d756ab2a8fb669ba84dbac189
parameshwaran.N <109024881+parameshwaran-46@users.noreply.github.com>
2022-07-10 17:47:06 +0530

    Add files via upload

---
+++ max&min.py
@@ -0,0 +1,12 @@
+num1= int(input("enter first number: "))
+num2= int(input("enter second number: "))
+def min(a,b):
+    if a>b:
+        return b
+    else:
+        return a
+def max(a,b):
+    if a<b:
+        return b
+    else:
+        return a
